meego-netbook-ux/anerley

commit 88c3419b9850625464adc16ee230144e019a357c

Updates for API changes in nbtk-1.2

anerley/anerley-tp-feed.c
@@ -354,7 +354,7 @@ _tp_connection_get_contacts_cb (TpConnection      *connection,
     if (tp_contact_get_presence_type (contact) == TP_CONNECTION_PRESENCE_TYPE_OFFLINE)
     {
       /* Skip offline contacts */
-      continue;
+      //continue;
     }
 
     item = g_hash_table_lookup (priv->ids_to_items,

anerley.pc.in
@@ -8,6 +8,6 @@ Description: Anerley people widgets
 Version: @VERSION@
 Libs: -L${libdir} -lanerley
 Cflags: -I${includedir}
-Requires: glib-2.0 dbus-glib-1 telepathy-glib libmissioncontrol nbtk-1.0 libebook-1.2
+Requires: glib-2.0 dbus-glib-1 telepathy-glib libmissioncontrol nbtk-1.2 libebook-1.2
 
 

anerley/anerley-tile-view.c
@@ -261,6 +261,7 @@ anerley_tile_view_init (AnerleyTileView *self)
 
   nbtk_grid_set_row_gap (NBTK_GRID (self), ROW_SPACING);
   nbtk_grid_set_column_gap (NBTK_GRID (self), COL_SPACING);
+  g_object_set (self, "fixed-size-children", TRUE, NULL);
 }
 
 NbtkWidget *

anerley/anerley-tile.c
@@ -100,8 +100,7 @@ _get_default_avatar_texture (void)
 
   cache = nbtk_texture_cache_get_default ();
   tmp_tex = nbtk_texture_cache_get_texture (cache,
-                                            DEFAULT_AVATAR_IMAGE,
-                                            FALSE);
+                                            DEFAULT_AVATAR_IMAGE);
   handle = clutter_texture_get_cogl_texture (tmp_tex);
   cogl_handle_ref (handle);
   g_object_unref (tmp_tex);
@@ -151,8 +150,7 @@ anerley_tile_update_presence_icon (AnerleyTile *tile,
   cache = nbtk_texture_cache_get_default ();
 
   priv->presence_icon = (ClutterActor *)nbtk_texture_cache_get_texture (cache,
-                                                                        path,
-                                                                        FALSE);
+                                                                        path);
   clutter_actor_set_size (priv->presence_icon, 16, 16);
   nbtk_table_add_actor_with_properties (NBTK_TABLE (tile),
                                         (ClutterActor *)priv->presence_icon,
@@ -477,8 +475,7 @@ anerley_tile_init (AnerleyTile *self)
   priv->avatar_bin = nbtk_bin_new ();
   nbtk_widget_set_style_class_name (priv->avatar_bin, "AnerleyTileAvatar");
 
-  priv->avatar = g_object_new (PENGE_TYPE_MAGIC_TEXTURE,
-                               NULL);
+  priv->avatar = g_object_new (PENGE_TYPE_MAGIC_TEXTURE, NULL);
   /* TODO: Prefill with unknown icon */
   nbtk_bin_set_child (NBTK_BIN (priv->avatar_bin), priv->avatar);
 
@@ -489,10 +486,10 @@ anerley_tile_init (AnerleyTile *self)
                                (ClutterActor *)priv->avatar_bin, FALSE);
   nbtk_table_child_set_y_fill (NBTK_TABLE (self),
                                (ClutterActor *)priv->avatar_bin, FALSE);
-  nbtk_table_child_set_col_expand (NBTK_TABLE (self),
-                                   (ClutterActor *)priv->avatar_bin, FALSE);
-  nbtk_table_child_set_row_expand (NBTK_TABLE (self),
-                                   (ClutterActor *)priv->avatar_bin, FALSE);
+  nbtk_table_child_set_x_expand (NBTK_TABLE (self),
+                                 (ClutterActor *)priv->avatar_bin, FALSE);
+  nbtk_table_child_set_y_expand (NBTK_TABLE (self),
+                                 (ClutterActor *)priv->avatar_bin, FALSE);
   nbtk_table_child_set_row_span (NBTK_TABLE (self),
                                  (ClutterActor *)priv->avatar_bin, 3);
 
@@ -504,8 +501,8 @@ anerley_tile_init (AnerleyTile *self)
                         0, 1);
   nbtk_table_child_set_y_fill (NBTK_TABLE (self),
                                (ClutterActor *)priv->primary_label, FALSE);
-  nbtk_table_child_set_col_expand (NBTK_TABLE (self),
-                                   (ClutterActor *)priv->primary_label, FALSE);
+  nbtk_table_child_set_x_expand (NBTK_TABLE (self),
+                                 (ClutterActor *)priv->primary_label, FALSE);
   nbtk_table_child_set_col_span (NBTK_TABLE (self),
                                  (ClutterActor *)priv->primary_label, 2);
   nbtk_table_child_set_y_align (NBTK_TABLE (self),
@@ -522,11 +519,11 @@ anerley_tile_init (AnerleyTile *self)
   nbtk_table_child_set_y_fill (NBTK_TABLE (self),
                                (ClutterActor *)priv->secondary_label,
                                FALSE);
-  nbtk_table_child_set_row_expand (NBTK_TABLE (self),
-                                   (ClutterActor *)priv->secondary_label,
-                                   FALSE);
-  nbtk_table_child_set_col_expand (NBTK_TABLE (self),
-                                   (ClutterActor *)priv->secondary_label,
+  nbtk_table_child_set_y_expand (NBTK_TABLE (self),
+                                 (ClutterActor *)priv->secondary_label,
+                                 FALSE);
+  nbtk_table_child_set_x_expand (NBTK_TABLE (self),
+                                 (ClutterActor *)priv->secondary_label,
                                    FALSE);
   nbtk_table_child_set_col_span (NBTK_TABLE (self),
                                  (ClutterActor *)priv->secondary_label, 2);

configure.ac
@@ -26,7 +26,7 @@ CFLAGS="$CFLAGS -Wall"
 PKG_CHECK_MODULES(GLIB, glib-2.0 >= 2.14 gobject-2.0)
 PKG_CHECK_MODULES(TELEPATHY, telepathy-glib)
 PKG_CHECK_MODULES(MC, libmissioncontrol)
-PKG_CHECK_MODULES(NBTK, nbtk-1.0 >= 0.15.0)
+PKG_CHECK_MODULES(NBTK, nbtk-1.2)
 PKG_CHECK_MODULES(EBOOK, libebook-1.2)
 
 AC_OUTPUT([

commit daeb341551b02571132bdc23fd4c04b1174be807

[AnerleyTpFeed] Fix regression introduced in 88c3419b
Fixes: MB#6362

anerley/anerley-tp-feed.c
@@ -354,7 +354,7 @@ _tp_connection_get_contacts_cb (TpConnection      *connection,
     if (tp_contact_get_presence_type (contact) == TP_CONNECTION_PRESENCE_TYPE_OFFLINE)
     {
       /* Skip offline contacts */
-      //continue;
+      continue;
     }
 
     item = g_hash_table_lookup (priv->ids_to_items,